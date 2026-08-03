2887f4ac2a7d35ad390d0f12f4ba686ef92d53bb
rcruz.eu <rcruz.eu@gmail.com>
2026-08-03 17:17:21 +0100

    Add files via upload

--- articles.js
+++ articles.js
@@ -1,7 +1,7 @@
 const ARTICLES = [
-   {
-     title: "How To: Build an OutSystems.AI Chatbot with Azure Bot Framework",
-     date: "2020-06-04",
-     url: "https://medium.com/noesis-low-code-solutions/outsystems-ai-chatbot-with-azure-bot-framework-a2f6176d288d"
-   },
+ {
+   title: "How To: Build an OutSystems.AI Chatbot with Azure Bot Framework",
+   date: "2020-06-04",
+   url: "https://medium.com/noesis-low-code-solutions/outsystems-ai-chatbot-with-azure-bot-framework-a2f6176d288d"
+ },
 ];
