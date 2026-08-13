4d35b0ce2ef189fb6430a923ee01d9aced5f9d41
rcruz.eu <rcruz.eu@gmail.com>
2026-08-13 23:47:20 +0100

    Add new article on ETags in Action

--- articles.js
+++ articles.js
@@ -1,5 +1,10 @@
 const ARTICLES = [
   {
+    title: "ETags in Action: Definition, Benefits, and Application",
+    date: "2025-01-15",
+    url: "articles/etags-in-action.html"
+  },
+  {
     title: "JWT: Secure and Scalable Authentication",
     date: "2024-04-04",
     url: "articles/jwt-secure-scalable-authentication.html"
